641182af9a077be29b681ba7386879309088653b
gdwb3219 <gdwb3219@gmail.com>
2023-03-12 18:01:30 +0900

    Apply Style Css File

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import { useState } from 'react';
+import Board from './Board';
 import Button from './Button';
-import Dice from './Dice';
+import './App.css';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
 }
 
 function App() {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherHistory] = useState([]);
 
   const handleRollDice = () => {
     const nextNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum);
-    setGameHistory([...gameHistory, nextNum]);
+    const nextOtherNum = random(6);
+    setMyHistory([...myHistory, nextNum]);
+    setOtherHistory([...otherHistory, nextOtherNum]);
   };
 
   const handleRollClear = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
+    setMyHistory([]);
+    setOtherHistory([]);
   };
 
   return (
-    <div>
+    <div className="App">
       <div>
-        <Button onClick={handleRollDice}>던지기</Button>
-        <Button onClick={handleRollClear}>처음부터</Button>
+        <Button className="App-button" color="blue" onClick={handleRollDice}>
+          던지기
+        </Button>
+        <Button className="App-button" color="red" onClick={handleRollClear}>
+          처음부터
+        </Button>
       </div>
       <div>
-        <Dice color="blue" num={num} />
-        <h1>총점</h1>
-        <h3>{sum}</h3>
-        <h1>기록</h1>
-        <h3>{gameHistory.join(', ')}</h3>
+        <Board name="나" color="blue" gameHistory={myHistory} />
+        <Board name="상대" color="red" gameHistory={otherHistory} />
       </div>
     </div>
   );
